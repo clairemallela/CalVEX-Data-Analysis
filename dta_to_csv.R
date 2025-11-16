6ee4e2a44134c48cacb4ff85119da146de1a94e7
Claire Mallela <mallelaclaire@gmail.com>
2025-11-16 15:50:45 -0800

    Updated folders.

--- dta_to_csv.R
+++
@@ -1,6 +0,0 @@
-# calvex_venv/Scripts/activate
-
-library(haven)
-
-CalVEX2021_csv <- read_dta('C:\\Users\\ravim\\Desktop\\CalVEX\\raw_data\\CalVEX2021_dta.dta')
-write.csv(CalVEX2021_csv, file = "CalVEX2021_csv.csv")
